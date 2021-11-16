ecf2059157debe6074969eca775ea6dff0b27ba2
Hamza Ghelle <hamzaghellegmail.com>
2021-11-16 17:20:02 -0500

    added cabybara and poltergeist

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require "capybara/rails"
+require "capybara/rspec"
+require "capybara/poltergeist"
 ENV['RAILS_ENV'] ||= 'test'
+Capybara.javascript_driver = :poltergeist
 
 require File.expand_path('../config/environment', __dir__)
 
@@ -22,7 +26,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
